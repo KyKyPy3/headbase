a334e531c5d959237acbfe2dbe27b8785ac3dcb3
Roman Efremenko <roman.efremenko@infowatch.com>
2011-08-08 14:27:39 +0400

    Initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,22 @@
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+
+$(document).ready(function() {
+  $('#navigation a').stop().animate({'marginLeft':'85px'},1000);
+
+  $('#navigation > li').hover(
+    function () {
+        $('a',$(this)).stop().animate({'marginLeft':'10px'},500);
+    },
+    function () {
+        $('a',$(this)).stop().animate({'marginLeft':'85px'},200);
+    }
+  );
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nerdbase::Application.config.secret_token = '5bb3d0ef2202632986987582c7307c31f9664503e3e93a0a1bcd97b6f6f576d67c27cff86636e4a37ca84f8f5dfc896dd5ac0f16b3c1b3542f1dde87cccd53c6'
