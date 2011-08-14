568b5262d178f64b641ec8251b8983f4df2c725c
Roman Efremenko <zi81@nm.ru>
2011-08-14 21:56:17 +0400

    some minor changes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  layout "application", :except => [:tab1, :tab2, :general]
+  layout "application", :except => [:tab1, :tab2, :general, :browse]
   
   def index
   end
