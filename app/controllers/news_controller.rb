b951f6fde370d05e91bec8fc888ef3778c03d069
Roman Efremenko <zi81@nm.ru>
2011-08-16 00:31:32 +0400

    Some fix to pagination

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,7 +2,7 @@ class NewsController < ApplicationController
   # GET /news
   # GET /news.json
   def index
-    @news = News.all.paginate(:page => params[:page], :per_page => 10)
+    @news = News.paginate(:page => params[:page], :per_page => 10)
 
     respond_to do |format|
       format.html # index.html.erb
