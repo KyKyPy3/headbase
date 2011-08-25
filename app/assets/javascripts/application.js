5d8d1afcad7458b44e3c0c0f667497922f3831d6
Roman Efremenko <zi81@nm.ru>
2011-08-25 23:17:35 +0400

    Project changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,85 @@
 //= require jquery_ujs
 //= require rails.validations
 //= require_tree .
+
+
+
+var tabs = {
+	"master" : {
+	},
+	
+	"branch 1": {
+	},
+	
+	"branch 2": {
+	},
+	
+	"branch 3" : {
+	},
+	
+	"last branch": {
+	}
+}
+
+var totalTabs;
+$(document).ready(function(){
+	totalTabs=0;
+	$.each(tabs,function(){totalTabs++;})
+	
+
+	$('#feedWidget').show().mouseleave(function(){
+		$('.dropDownList').remove();
+		$('#activeTab').removeClass('hover');
+	}).mouseenter(function(){
+		if(totalTabs>1) $('#activeTab').addClass('hover');
+	});
+
+	$('#activeTab').click(showDropDown);
+
+	$('.dropDownList div').live('click',function(){
+		showDropDown();
+		showTab($(this).text());
+		$('#activeTab').removeClass('hover');
+	});
+	
+	showTab('master');
+});
+
+
+function showTab(key)
+{
+	var obj = tabs[key];
+	if(!obj) return false;
+
+	$('#activeTab').text(key);
+}
+
+function showDropDown()
+{
+	if(totalTabs<2) return false;
+	
+	if($('#feedWidget .dropDownList').length)
+	{
+		$('.dropDownList').slideUp('fast',function(){ $(this).remove(); })
+		return false;
+	}
+	
+	var activeTab = $('#activeTab');
+	
+	var offsetTop = (activeTab.offset().top - $('#feedWidget').offset().top )+activeTab.outerHeight() - 5;
+	
+	var dropDown = $('<div>').addClass('dropDownList').css({
+
+			'top'	: offsetTop,
+			'width'	: activeTab.width()
+	
+	}).hide().appendTo('#feedWidget')
+	
+	$.each(tabs,function(j){
+		if(j==activeTab.text()) return true;
+		
+			$('<div>').text(j).appendTo(dropDown);
+	})
+	
+	dropDown.slideDown('fast');
+}
