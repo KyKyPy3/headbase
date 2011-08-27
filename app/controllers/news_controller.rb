94b1ce42dee1ddc09c72bafcc8a0023d2888dffb
Roman Efremenko <zi81@nm.ru>
2011-08-28 00:49:52 +0400

    Add auth

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,12 +1,12 @@
 class NewsController < ApplicationController
+  before_filter :authenticate_user!, :except => [:index, :show]
+  
   # GET /news
-  # GET /news.json
   def index
     @news = News.paginate(:page => params[:page], :per_page => 10)
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @news }
     end
   end
 
